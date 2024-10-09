0b468f5e8f239408954c5fadf1d9e5a790c0ed0e
Nicolas LEFEBVRE <nlv@3ds.com>
2024-10-09 11:36:17 -0400

    Add a type guard to differentiate admins from users, show person's job / admin role

--- src/main.ts
+++ src/main.ts
@@ -16,15 +16,25 @@ interface Admin {
 
 type Person = User | Admin
 
+function isAdmin(person: Person): person is Admin {
+  return (person as Admin).role !== undefined
+}
+
 const people: Person[] = [
   { name: 'Toto Dupont', age: 35, occupation: 'Postman' },
   { name: 'Jeanne Doe', age: 25, role: 'Admin' },
 ]
 
 function getPersonDescription(person: Person): string {
-  return `${person.name} is ${person.age} years old`
+  if (isAdmin(person)) {
+    return `${person.name} is ${person.age} years old; they are a superuser with the role of ${person.role}`
+  } else {
+    return `${person.name} is ${person.age} years old; they are a user and their occupation is ${person.occupation}`
+  }
 }
 
 for (let person of people) {
   console.log(getPersonDescription(person))
 }
+
+
